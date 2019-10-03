da9aa76c2fa28cfd3a8be994a7f73dc0948fc66a
hector.limaaa@gmail.com <385091229@alunos.etec.itaqua>
2019-10-02 22:33:16 -0300

    criação do ler livro

---
+++ index.js
@@ -0,0 +1,14 @@
+$(function(){
+  function carregarLivros(){
+    fetch("http://localhost:8080/bookteca-api/src/lerlivros.php").then(function(resposta){
+      resposta.json().then(function(json){
+        let listagem = $("#listagem")
+        let coluna4 = $("<div></div>")
+        coluna4.addClass("col-4")
+        coluna4.append(json[0].titulo)
+        listagem.append(coluna4)
+      })
+    })
+  }
+  carregarLivros()
+})
